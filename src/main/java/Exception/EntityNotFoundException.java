716fa624f93fc95b8d16374ac3ed40bd56ecc4df
BILAL <hasbilal92@gmail.com>
2021-02-28 08:49:46 +0100

    Execption

---
+++ src/main/java/Exception/EntityNotFoundException.java
@@ -0,0 +1,28 @@
+package Exception;
+
+import lombok.Getter;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    @Getter
+    private CodeErreur codeErreur;
+
+    public EntityNotFoundException(String message){
+        super(message);
+    }
+
+    public EntityNotFoundException(String message, Throwable cause){
+        super(message,cause);
+    }
+
+    public EntityNotFoundException(String message, Throwable cause,CodeErreur codeErreur){
+        super(message,cause);
+        this.codeErreur=codeErreur;
+    }
+
+    public EntityNotFoundException(CodeErreur codeErreur){
+        this.codeErreur=codeErreur;
+    }
+
+
+}
---
+++ src/main/java/Exception/InvalideEntityException.java
@@ -0,0 +1,39 @@
+package Exception;
+
+
+import lombok.Getter;
+
+import java.util.List;
+
+public class InvalideEntityException extends RuntimeException {
+
+    @Getter
+    private CodeErreur codeErreur;
+    @Getter
+    private List<String> erreurs;
+
+
+    public InvalideEntityException(String message){
+        super(message);
+    }
+
+    public InvalideEntityException(String message, Throwable cause){
+        super(message,cause);
+    }
+
+    public InvalideEntityException(String message, Throwable cause, CodeErreur codeErreur){
+        super(message,cause);
+        this.codeErreur=codeErreur;
+    }
+
+    public InvalideEntityException(String message, CodeErreur codeErreur){
+        super(message);
+        this.codeErreur=codeErreur;
+    }
+
+    public InvalideEntityException(String message,CodeErreur codeErreur, List<String>erreurs){
+        super(message);
+        this.codeErreur=codeErreur;
+        this.erreurs=erreurs;
+    }
+}
